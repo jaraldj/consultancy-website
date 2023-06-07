e0587bf1869e01b78b202650a0d3670046e737b5
jaraldj <jarald.jsj@gmail.com>
2023-06-07 15:44:05 +0530

    Contents Change and responsive pb change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'backimg':
+          'url("https://images.unsplash.com/photo-1611078489935-0cb964de46d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80")',
+      },
+    },
   },
   plugins: [],
-}
-
+};
